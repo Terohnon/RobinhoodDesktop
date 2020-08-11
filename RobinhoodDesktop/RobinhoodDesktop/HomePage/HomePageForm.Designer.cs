55797cccdaa2e73d9eb657e6a282f7857f9a36f3
Terohnon <terohnon@gmail.com>
2020-08-10 23:21:59 -0400

    Removed x86 build since for some reason it was forcing that one to execute instead of the selected x64

--- RobinhoodDesktop/RobinhoodDesktop/HomePage/HomePageForm.Designer.cs
+++ RobinhoodDesktop/RobinhoodDesktop/HomePage/HomePageForm.Designer.cs
@@ -28,18 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.SuspendLayout();
-			// 
-			// HomePageForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-			this.ClientSize = new System.Drawing.Size(2128, 1404);
-			this.Margin = new System.Windows.Forms.Padding(6);
-			this.Name = "HomePageForm";
-			this.Text = "Robinhood Desktop";
-			this.ResumeLayout(false);
+            this.SuspendLayout();
+            // 
+            // HomePageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1264, 762);
+            this.Name = "HomePageForm";
+            this.Text = "Robinhood Desktop";
+            this.ResumeLayout(false);
 
         }
 
